e68ed9732f4a9c3d1e386dfba78f479687ab3ebb
tsufeki <tsufeki@ymail.com>
2018-01-21 21:22:01 +0100

    Add logging option.

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: ExtensionContext) {
 
     const serverOptions: ServerOptions = {
         command: phpExecutable,
-        args: [serverExecutable],
+        args: [serverExecutable, '--log=tenkawa.log'],
     };
 
     const clientOptions: LanguageClientOptions = {
