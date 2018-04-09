3f2cdad963d5e0beece7171bb37dff6f0573f932
tsufeki <tsufeki@ymail.com>
2018-04-09 23:45:27 +0200

    Revert to standard 'composer install'. Improve error handling.

--- src/extension.ts
+++ src/extension.ts
@@ -3,54 +3,62 @@
 import * as net from 'net';
 import * as path from 'path';
 import { spawn } from 'child_process';
-import { ExtensionContext, Disposable, workspace } from 'vscode';
+import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, generateRandomPipeName, StreamInfo } from 'vscode-languageclient';
 
-export function activate(context: ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 
-    const config = workspace.getConfiguration('tenkawaphp');
+    const config = vscode.workspace.getConfiguration('tenkawaphp');
     const phpExecutable = config.get<string>('executablePath') || 'php';
-    const serverExecutable = context.asAbsolutePath(path.join('tenkawa-php-language-server', 'bin', 'tenkawa.php'));
+    const serverExecutable = context.asAbsolutePath(path.join('vendor', 'bin', 'tenkawa.php'));
     const memoryLimit = '1024M';
-    const logDestination = context.asAbsolutePath('tenkawa-php.log');
 
     const args = [
         '-dmemory_limit=' + memoryLimit,
         serverExecutable,
-        '--log=' + logDestination,
+        '--log-stderr',
+        '--log-level=info',
     ];
 
     let serverOptions: ServerOptions;
-    if (process.platform !== 'win32') {
-        serverOptions = {
-            command: phpExecutable,
-            args,
-        };
-    } else {
-        serverOptions = () => new Promise<StreamInfo>((resolve, reject) => {
-            const server = net.createServer(socket => {
-                server.close();
-                resolve({ reader: socket, writer: socket });
-            });
-
-            server.listen(0, '127.0.0.1', () => {
-                const childProcess = spawn(
-                    phpExecutable,
-                    args.concat([`--socket=tcp://${server.address().address}:${server.address().port}`]),
-                );
-
-                childProcess.stderr.on('data', (chunk: Buffer) => {
-                    console.error(chunk + '');
-                });
-                childProcess.stdout.on('data', (chunk: Buffer) => {
-                    console.error(chunk + '');
-                });
-            });
+    serverOptions = () => new Promise<StreamInfo>((resolve, reject) => {
+        const server = net.createServer(socket => {
+            server.close();
+            resolve({ reader: socket, writer: socket });
         });
-    }
+
+        server.listen(0, '127.0.0.1', () => {
+            const childProcess = spawn(
+                phpExecutable,
+                args.concat([`--socket=tcp://${server.address().address}:${server.address().port}`]),
+            );
+
+            childProcess.stderr.on('data', chunk => {
+                client.outputChannel.append(chunk + '');
+            });
+
+            childProcess.stdout.on('data', chunk => {
+                client.outputChannel.append(chunk + '');
+            });
+
+            let showError = () => {
+                vscode.window.showErrorMessage(
+                    'PHP language server could not be started. ' +
+                    'Please set "tenkawaphp.executablePath" option to a PHP>=7.0 executable'
+                );
+                showError = () => {};
+            };
+
+            childProcess.on('error', error => showError());
+            childProcess.on('exit', (code, signal) => showError());
+        });
+    });
 
     const clientOptions: LanguageClientOptions = {
-        documentSelector: ['php'],
+        documentSelector: [
+            { scheme: 'file', language: 'php' },
+            { scheme: 'untitled', language: 'php' }
+        ],
     };
 
     const client = new LanguageClient('Tenkawa PHP', serverOptions, clientOptions);
